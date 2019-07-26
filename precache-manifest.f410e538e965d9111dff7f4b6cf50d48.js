9addca6d6ca14111f2525394f4a0c45ee51ddd26
Marco Mazzai <mazzai.marco@gmail.com>
2019-07-26 11:55:44 +0200

    Updates

---
+++ precache-manifest.f410e538e965d9111dff7f4b6cf50d48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c58f42cd7ffb4686c234070c1f99e5",
+    "url": "/celsius-fahrenheit-converter-react/index.html"
+  },
+  {
+    "revision": "9a25532fce6dfd2a4d32",
+    "url": "/celsius-fahrenheit-converter-react/static/css/main.7504fc08.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/celsius-fahrenheit-converter-react/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "9a25532fce6dfd2a4d32",
+    "url": "/celsius-fahrenheit-converter-react/static/js/main.18e4a015.chunk.js"
+  },
+  {
+    "revision": "4bfcb349920068315d39",
+    "url": "/celsius-fahrenheit-converter-react/static/js/runtime~main.1621b8a7.js"
+  }
+]);
